80777ddb36bf40ba9a83bc61c4bd494328d2b46e
AlexanderElkin1969 <eaa@mfcmmr.ru>
2023-08-06 02:31:27 +0300

    homework re-redone

--- src/main/java/pro/sky/java/course2/newStore/controllers/NewStoreController.java
+++ src/main/java/pro/sky/java/course2/newStore/controllers/NewStoreController.java
@@ -1,10 +1,11 @@
 package pro.sky.java.course2.newStore.controllers;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import pro.sky.java.course2.newStore.model.Product;
-import pro.sky.java.course2.newStore.service.NewStoreService;
+import pro.sky.java.course2.newStore.model.*;
 import pro.sky.java.course2.newStore.exceptions.*;
+import pro.sky.java.course2.newStore.service.NewStoreServiceImpl;
 
 import java.util.List;
 import java.util.Map;
@@ -12,15 +13,15 @@ import java.util.Map;
 @RestController
 @RequestMapping("/store")
 public class NewStoreController {
-    private final NewStoreService storeService;
+    private final NewStoreServiceImpl newStoreService;
 
-    public NewStoreController(NewStoreService storeService) {
-        this.storeService = storeService;
+    public NewStoreController(NewStoreServiceImpl newStoreService) {
+        this.newStoreService = newStoreService;
     }
 
-    @GetMapping()
+    @GetMapping(produces = MediaType.TEXT_HTML_VALUE)
     public String hello() {
-        return "Добро пожаловать в наш интернет-магазин.  " +
+        return "<h1 style = \" color: green \">Добро пожаловать в наш интернет-магазин.</h1>" +
                 "Для просмотра каталога товара воспользуйтесь командой /order.  " +
                 "Для выбора товара воспользуйтесь командой /order/add?ID=Артикль_товара&quantity=количество_товара.  " +
                 "Для просмотра выбранного товара воспользуйтесь командой /order/get.  ";
@@ -28,18 +29,18 @@ public class NewStoreController {
 
     @GetMapping(path = "/order")
     public Map<String, Product> getList() {
-        return storeService.getList();
+        return newStoreService.getList();
     }
 
     @GetMapping(path = "/order/add")
     public String addProductToBasket(@RequestParam("ID") String id,
                                      @RequestParam("quantity") int quantity) {
-        return storeService.add(id, quantity).toString() + " добавлен в корзину.";
+        return newStoreService.add(id, quantity).toString() + " добавлен в корзину.";
     }
 
     @GetMapping(path = "/order/get")
     public List<Product> getProductToBasket() {
-        return storeService.get();
+        return newStoreService.get();
     }
 
     @ExceptionHandler(LittleValueProductException.class)
